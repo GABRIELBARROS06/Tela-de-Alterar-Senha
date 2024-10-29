94e2c7e2178afd570231f65e2c164510ce7047ed
GABRIEL DE BARROS GOMES <23111782@dsn127m1242375.cfp127aluno.local>
2024-10-29 14:37:06 -0300

    Feito

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+import { FaEye } from "react-icons/fa";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+
+      <header>
+
+        <MdOutlineKeyboardArrowLeft  className='icon'/>
+
+        <h2>Alterar Senha</h2>
+
       </header>
+
+      <div className='mesage'>
+
+        <spam>
+          <p>
+            Um código de verificação foi enviado ao seu email.
+            Por favor, insira-o no campo abaixo
+          </p>
+        </spam>
+
+<div className="mesage-input">
+<input className='code' disabled placeholder='AH293HN'></input>
+
+<button className='send'>
+
+  <IoMdSend />
+
+</button>
+</div>
+
+        </div>
+
+        <p className='text'>
+          Sua senha precisa ter pelo menos seis caracteres e
+          incluir uma combinação de números, letras e caracteres especiais
+          !@#$
+        </p>
+
+        <div className='passwords'>
+          <input className='pass' type="password"></input>
+          <input className='pass1' type="password">
+          </input>
+          <FaEye className='eye'/>
+
+          <button>
+            <p>Confirmar</p>
+          </button>
+        </div>
+
+
     </div>
   );
 }
